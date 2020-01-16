3c6611a599c2050eae01ff2cb5641d0d6aea91de
jmrada14 <jmrada14@tutanota.com>
2020-01-16 11:43:13 -0500

    more routes added

--- fastify-server/controllers/memberController.js
+++ fastify-server/controllers/memberController.js
@@ -23,3 +23,78 @@ exports.getMembersByState = async (req, res) => {
         throw boom.boomify(err)
     }
 };
+
+exports.getMembersByChamber = async (req, res) => {
+    try{
+        const chamber = req.params.chamber;
+        const member = await Member.find({chamber: chamber})
+        return member
+    }catch (err) {
+        throw boom.boomify(err)
+    }
+};
+
+exports.getMembersByParty = async (req, res) => {
+    try{
+        const party = req.params.party;
+        const member = await Member.find({party: party})
+        return member
+    }catch (err) {
+        throw boom.boomify(err)
+    }
+};
+
+exports.getMembersByChamberAndState = async (req, res) => {
+    try{
+        const chamber = req.params.chamber;
+        const state = req.params.state;
+        const member = await Member.find({chamber: chamber , state: state})
+        return member
+    }catch (err) {
+        throw boom.boomify(err)
+    }
+};
+
+exports.getMembersByChamberAndParty = async (req, res) => {
+    try{
+        const chamber = req.params.chamber;
+        const party = req.params.party;
+        const member = await Member.find({chamber: chamber , party: party})
+        return member
+    }catch (err) {
+        throw boom.boomify(err)
+    }
+};
+
+exports.getMembersByChamberAndGender = async (req, res) => {
+    try{
+        const chamber = req.params.chamber;
+        const gender = req.params.gender;
+        const member = await Member.find({chamber: chamber , gender: gender})
+        return member
+    }catch (err) {
+        throw boom.boomify(err)
+    }
+};
+
+exports.getMembersByPartyAndState = async (req, res) => {
+    try{
+        const party = req.params.party;
+        const state = req.params.state;
+        const member = await Member.find({party: party, state: state})
+        return member
+    }catch (err) {
+        throw boom.boomify(err)
+    }
+};
+
+exports.getMembersByGenderAndParty = async (req, res) => {
+    try{
+        const gender = req.params.gender;
+        const party = req.params.party;
+        const member = await Member.find({gender: gender , party: party})
+        return member
+    }catch (err) {
+        throw boom.boomify(err)
+    }
+};
