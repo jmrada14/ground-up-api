7760bbc1a40856c198fc7492b0828bc6cafa3518
jmrada14 <jmrada14@tutanota.com>
2020-01-16 11:52:05 -0500

    new server ready, still need bills and more info, but controllers work just fine

--- fastify-server/routes/index.js
+++ fastify-server/routes/index.js
@@ -4,14 +4,49 @@ const todoController = require("../controllers/memberController");
 const routes = [
     {
         method: 'GET',
-        url: '/api/members',
+        url: '/api/members/all',
         handler: todoController.getMembers
     },
     {
         method: 'GET',
         url: '/api/members/state/:state',
         handler: todoController.getMembersByState
-    }
+    },
+    {
+        method: 'GET',
+        url: '/api/members/party/:party',
+        handler: todoController.getMembersByParty
+    },
+    {
+        method: 'GET',
+        url: '/api/members/chamber/:chamber',
+        handler: todoController.getMembersByChamber
+    },
+    {
+        method: 'GET',
+        url: '/api/members/chamber/:chamber/state/:state',
+        handler: todoController.getMembersByChamberAndState
+    },
+    {
+        method: 'GET',
+        url: '/api/members/chamber/:chamber/party/:party',
+        handler: todoController.getMembersByChamberAndParty
+    },
+    {
+        method: 'GET',
+        url: '/api/members/chamber/:chamber/gender/:gender',
+        handler: todoController.getMembersByChamberAndGender
+    },
+    {
+        method: 'GET',
+        url: '/api/members/party/:party/state/:state',
+        handler: todoController.getMembersByPartyAndState
+    },
+    {
+        method: 'GET',
+        url: '/api/members/gender/:gender/party/:party',
+        handler: todoController.getMembersByGenderAndParty
+    },
     ];
 
 module.exports = routes;
